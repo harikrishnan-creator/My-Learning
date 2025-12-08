26382403b63287001e133c001a2a6ac125f268d3
harikrishnan-creator <78860755+harikrishnan-creator@users.noreply.github.com>
2025-12-08 07:33:31 -0500

    SpringBoot CRUD project

---
+++ demo.crudRepo/src/test/java/com/example/Learnings/demo/crudRepo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Learnings.demo.crudRepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
